eecd2376c7cc47eab4d388fe1da30fbe0cf7b738
Laura Goon <goon.laura@gmail.com>
2022-12-03 23:49:27 -0800

    add feature normalization & feature selection

--- script.py
+++ script.py
@@ -1,6 +1,9 @@
 # import numpy as np
 # import math
 import pandas as pd
+from sklearn import datasets
+from sklearn.feature_selection import chi2, SelectKBest
+from sklearn.preprocessing import MinMaxScaler
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
@@ -16,14 +19,14 @@ TRAINING_PLAYLIST_IDS = {"Hip Hop": "37i9dQZF1EQnqst5TRi17F", "Pop": "37i9dQZF1E
                          "Soul": "37i9dQZF1EQntZpEGgfBif", "Punk": "37i9dQZF1EQqlvxWrOgFZm",
                          "Blues": "37i9dQZF1EQpz3DZCEoX3g", "Afrobeats": "37i9dQZF1EQqFPe2ux3rbj",
                          "Funk": "37i9dQZF1EQnJyHBkXpASl"}
-TRAINING_DATA, TESTING_DATA = None, None
+NUM_FEATURES = 5
 
 SP = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET))
 
 
-def get_training_track_names(playlist_ids):
+def get_track_names(playlist_ids):
     track_names_df = pd.DataFrame(columns=["id", "name", "genre"])
 
     for genre_name, genre_id in playlist_ids.items():
@@ -39,7 +42,7 @@ def get_training_track_names(playlist_ids):
     return track_names_df
 
 
-def get_training_track_features(tracks):
+def get_track_features(tracks):
     ids = tracks["id"].tolist()
     num_processed = 0
     track_features_df = None
@@ -47,6 +50,7 @@ def get_training_track_features(tracks):
     while num_processed < len(tracks):
         end_idx = num_processed + 50 if num_processed + 50 < len(tracks) else len(tracks)
         track_features = SP.audio_features(ids[num_processed:end_idx])
+        track_features = list(filter(lambda item: item is not None, track_features)) # filter Nones
 
         if track_features_df is None:
             track_features_df = pd.DataFrame(track_features)
@@ -61,7 +65,7 @@ def get_training_track_features(tracks):
     return track_features_df
 
 
-def combine_training_track_data(track_names, track_features):
+def combine_track_data(track_names, track_features):
     track_names.drop_duplicates(subset=["id"], inplace=True)
     track_features.drop_duplicates(subset=["id"], inplace=True)
 
@@ -70,11 +74,43 @@ def combine_training_track_data(track_names, track_features):
     return combined_df
 
 
+def normalize_features(training_data):
+    """
+    Using Min-Max feature scaling method.
+    """
+    features_to_normalize = ["key", "loudness", "tempo", "duration_ms", "time_signature"]
+    training_data[features_to_normalize] = MinMaxScaler().fit_transform(training_data[features_to_normalize])
+
+    return training_data
+
+
+def select_features(training_data):
+    """
+    Using Chi-Squared test.
+    """
+    X = training_data.iloc[:, 3:]
+    y = training_data.iloc[:, 2:3]
+
+    test = SelectKBest(score_func=chi2, k=NUM_FEATURES)
+    test.fit(X, y)
+    cols = test.get_support(indices=True)
+    selected_features = training_data.iloc[:, cols+3]
+
+    return selected_features, training_data.iloc[:, 2:3], training_data.iloc[:, 0:2]
+
+
+def get_training_data(playlist_ids):
+    track_names = get_track_names(playlist_ids)
+    track_features = get_track_features(track_names)
+    combined_data = combine_track_data(track_names, track_features)
+    normalized_training_data = normalize_features(combined_data)
+    X, y, tags = select_features(normalized_training_data)
+
+    return X, y, tags
+
+
 def main():
-    training_track_names = get_training_track_names(TRAINING_PLAYLIST_IDS)
-    training_track_features = get_training_track_features(training_track_names)
-    global TRAINING_DATA
-    TRAINING_DATA = combine_training_track_data(training_track_names, training_track_features)
+    X_train, y_train, tags_train = get_training_data(TRAINING_PLAYLIST_IDS)
 
 
 main()
