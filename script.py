8cb341f54807eb8145a883cec881de4cc15f3128
Laura Goon <goon.laura@gmail.com>
2022-12-04 12:41:54 -0800

    enumerate genre class, perform some exploratory data viz

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 # import numpy as np
 # import math
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 from sklearn import datasets
 from sklearn.feature_selection import chi2, SelectKBest
 from sklearn.preprocessing import MinMaxScaler
@@ -19,6 +21,9 @@ TRAINING_PLAYLIST_IDS = {"Hip Hop": "37i9dQZF1EQnqst5TRi17F", "Pop": "37i9dQZF1E
                          "Soul": "37i9dQZF1EQntZpEGgfBif", "Punk": "37i9dQZF1EQqlvxWrOgFZm",
                          "Blues": "37i9dQZF1EQpz3DZCEoX3g", "Afrobeats": "37i9dQZF1EQqFPe2ux3rbj",
                          "Funk": "37i9dQZF1EQnJyHBkXpASl"}
+GENRE_CLASS = {"Hip Hop": 0, "Pop": 1, "Country": 2, "Latin": 3, "Rock": 4, "Dance/Electronic": 5, "Indie": 6, "R&B": 7,
+               "Christian": 8, "K-Pop": 9, "Metal": 10, "Jazz": 11, "Classical": 12, "Folk & Acoustic": 13, "Soul": 14,
+               "Punk": 15, "Blues": 16, "Afrobeats": 17, "Funk": 18}
 NUM_FEATURES = 5
 
 SP = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
@@ -27,7 +32,7 @@ SP = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
 
 
 def get_track_names(playlist_ids):
-    track_names_df = pd.DataFrame(columns=["id", "name", "genre"])
+    track_names_df = pd.DataFrame(columns=["id", "name", "genre", "genre_class"])
 
     for genre_name, genre_id in playlist_ids.items():
         response = SP.playlist_items(genre_id, fields=["items.track.id, items.track.name"])
@@ -35,10 +40,12 @@ def get_track_names(playlist_ids):
         for track in tracks_response:
             if track is not None and track["track"] is not None and track["track"]["id"] is not None:
                 track["track"]["genre"] = genre_name
+                track["track"]["genre_class"] = GENRE_CLASS[genre_name]
                 temp_df = pd.DataFrame([track["track"]])
                 track_names_df = pd.concat([temp_df, track_names_df])
 
-    track_names_df = track_names_df.astype({"id": "string", "name": "string", "genre": "string"})
+    track_names_df = track_names_df.astype({"id": "string", "name": "string", "genre": "string",
+                                            "genre_class": "int64"})
     return track_names_df
 
 
@@ -88,29 +95,45 @@ def select_features(training_data):
     """
     Using Chi-Squared test.
     """
-    X = training_data.iloc[:, 3:]
-    y = training_data.iloc[:, 2:3]
+    X = training_data.iloc[:, 4:]
+    y = training_data.iloc[:, 3:4]
 
     test = SelectKBest(score_func=chi2, k=NUM_FEATURES)
     test.fit(X, y)
     cols = test.get_support(indices=True)
-    selected_features = training_data.iloc[:, cols+3]
+    selected_features = training_data.iloc[:, cols+4]
 
-    return selected_features, training_data.iloc[:, 2:3], training_data.iloc[:, 0:2]
+    return selected_features, training_data.iloc[:, 3:4], training_data.iloc[:, 0:3]
 
 
-def get_training_data(playlist_ids):
+def get_data(playlist_ids):
     track_names = get_track_names(playlist_ids)
     track_features = get_track_features(track_names)
     combined_data = combine_track_data(track_names, track_features)
-    normalized_training_data = normalize_features(combined_data)
+
+    return combined_data
+
+
+def get_cleaned_data(df):
+    normalized_training_data = normalize_features(df)
     X, y, tags = select_features(normalized_training_data)
 
     return X, y, tags
 
 
+def visualize_data(df, filename="viz.png"):
+    plt.close()
+    sns.set_style("whitegrid")
+    sns_plot = sns.pairplot(df.drop(columns=["genre_class"], inplace=False).iloc[:, 2:], hue="genre", palette="tab10",
+                            height=9)
+    sns_plot.figure.savefig(filename)
+    # plt.show()
+
+
 def main():
-    X_train, y_train, tags_train = get_training_data(TRAINING_PLAYLIST_IDS)
+    training_data = get_data(TRAINING_PLAYLIST_IDS)
+    # visualize_data(training_data, "training_viz.png")
+    X_train, y_train, tags_train = get_cleaned_data(training_data)
 
 
 main()
