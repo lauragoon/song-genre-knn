5f727fcb04b64176a1b4d2f750d2ac3c25a80f9c
Laura Goon <goon.laura@gmail.com>
2022-12-03 20:31:36 -0800

    use diff training data to get genres, uses spotify-made Mix playlists

--- script.py
+++ script.py
@@ -6,8 +6,16 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 CLIENT_ID = ""
 CLIENT_SECRET = ""
-TRAINING_PLAYLIST_ID = "1C49yxU1XBkoq5yaVDbJwx"
-# TRAINING_PLAYLIST_IDS = {"Hip Hop": "1C49yxU1XBkoq5yaVDbJwx"}
+TRAINING_PLAYLIST_IDS = {"Hip Hop": "37i9dQZF1EQnqst5TRi17F", "Pop": "37i9dQZF1EQncLwOalG3K7",
+                         "Country": "37i9dQZF1EQmPV0vrce2QZ", "Latin": "37i9dQZF1EQmK1rjZuPGDt",
+                         "Rock": "37i9dQZF1EQpj7X7UK8OOF", "Dance/Electronic": "37i9dQZF1EQp9BVPsNVof1",
+                         "Indie": "37i9dQZF1EQqkOPvHGajmW", "R&B": "37i9dQZF1EQoqCH7BwIYb7",
+                         "Christian": "37i9dQZF1EQqZgBURAEzWH", "K-Pop": "37i9dQZF1EQpesGsmIyqcW",
+                         "Metal": "37i9dQZF1EQpgT26jgbgRI", "Jazz": "37i9dQZF1EQqA6klNdJvwx",
+                         "Classical": "37i9dQZF1EQn1VBR3CMMWb", "Folk & Acoustic": "37i9dQZF1EQp62d3Dl7ECY",
+                         "Soul": "37i9dQZF1EQntZpEGgfBif", "Punk": "37i9dQZF1EQqlvxWrOgFZm",
+                         "Blues": "37i9dQZF1EQpz3DZCEoX3g", "Afrobeats": "37i9dQZF1EQqFPe2ux3rbj",
+                         "Funk": "37i9dQZF1EQnJyHBkXpASl"}
 TRAINING_DATA, TESTING_DATA = None, None
 
 SP = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
@@ -15,25 +23,23 @@ SP = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
     client_secret=CLIENT_SECRET))
 
 
-def get_tracks(playlist_id):
-    response = SP.playlist_items(playlist_id, fields=["total"])
-    track_names_df = pd.DataFrame(columns=["id", "name"])
-    num_tracks = response["total"]
+def get_training_track_names(playlist_ids):
+    track_names_df = pd.DataFrame(columns=["id", "name", "genre"])
 
-    while track_names_df.shape[0] < num_tracks:
-        response = SP.playlist_items(playlist_id, fields=["items.track.id, items.track.name"],
-                                     offset=track_names_df.shape[0])
+    for genre_name, genre_id in playlist_ids.items():
+        response = SP.playlist_items(genre_id, fields=["items.track.id, items.track.name"])
         tracks_response = response["items"]
         for track in tracks_response:
             if track is not None and track["track"] is not None and track["track"]["id"] is not None:
+                track["track"]["genre"] = genre_name
                 temp_df = pd.DataFrame([track["track"]])
                 track_names_df = pd.concat([temp_df, track_names_df])
 
-    track_names_df = track_names_df.astype({"id": "string", "name": "string"})
+    track_names_df = track_names_df.astype({"id": "string", "name": "string", "genre": "string"})
     return track_names_df
 
 
-def get_tracks_features(tracks):
+def get_training_track_features(tracks):
     ids = tracks["id"].tolist()
     num_processed = 0
     track_features_df = None
@@ -55,7 +61,7 @@ def get_tracks_features(tracks):
     return track_features_df
 
 
-def combine_track_data(track_names, track_features):
+def combine_training_track_data(track_names, track_features):
     track_names.drop_duplicates(subset=["id"], inplace=True)
     track_features.drop_duplicates(subset=["id"], inplace=True)
 
@@ -65,10 +71,10 @@ def combine_track_data(track_names, track_features):
 
 
 def main():
-    training_track_names = get_tracks(TRAINING_PLAYLIST_ID)
-    training_track_features = get_tracks_features(training_track_names)
+    training_track_names = get_training_track_names(TRAINING_PLAYLIST_IDS)
+    training_track_features = get_training_track_features(training_track_names)
     global TRAINING_DATA
-    TRAINING_DATA = combine_track_data(training_track_names, training_track_features)
+    TRAINING_DATA = combine_training_track_data(training_track_names, training_track_features)
 
 
 main()
